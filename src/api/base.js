d375e2015e7490a392ce4f7c43bdc251086e864e
fandy <13319566928@163.com>
2021-02-28 17:31:58 +0800

    build

--- src/api/base.js
+++ src/api/base.js
@@ -2,8 +2,8 @@
  * 接口域名的管理
  */
 const base = {
-  // sq: "http://www.tukzi.cn:3000"
-  sq:'http://127.0.0.1:3000'
+  sq: "http://www.tukzi.cn:3000"
+  // sq:'http://127.0.0.1:3000'
   // 线上
   // sq: "/"
   // 本地
